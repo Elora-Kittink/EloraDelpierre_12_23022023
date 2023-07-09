482c4e53f757353ab734c2c599b3b28f58c91b0b
Elora Delpierre <edelpierre@exomind.fr>
2023-07-09 18:35:14 +0200

    login unit tests

---
+++ FosterAppTests/BaseTest/SignUpTests/SignUpTests.swift
@@ -0,0 +1,66 @@
+//
+//  SignUpTests.swift
+//  FosterAppTests
+//
+//  Created by Elora on 06/06/2023.
+//
+
+import XCTest
+import CoreDataUtilsKit
+@testable import FosterApp
+
+final class SignUpTests: XCTestCase {
+	
+	override func setUpWithError() throws {
+		try super.setUpWithError()
+		try? CoreDataManager.default.dropDatabase()
+	}
+	
+	struct MockUserWorker: UserWorkerProtocol {
+		func userConnected() async throws -> FosterApp.User? {
+			User(mail: "TEST", id: "ID", name: "TEST")
+		}
+		
+		func signUp(mail: String, password: String) async throws -> FosterApp.User {
+			User(mail: "TEST", id: "ID", name: "TEST")
+		}
+		
+		
+		func login(email: String, password: String) async throws -> FosterApp.User {
+			User(mail: "TEST", id: "ID", name: "TEST")
+		}
+	}
+	func testSuccessSignup() async throws {
+		
+		let test = await BaseTest<SignUpViewModel, SignUpPresenter, SignUpInteractor>()
+		
+		await test.fire { interactor in
+			DispatchQueue.main.async {
+				interactor.userWorker = MockUserWorker()
+				interactor.signUp(mail: "", name: "", password: "")
+			}
+		}
+		
+		DispatchQueue.main.async {
+			XCTAssertEqual(test.viewModel.userSignedIn?.id, "ID")
+			XCTAssertEqual(test.viewModel.userSignedIn?.mail, "TEST")
+			XCTAssertEqual(test.viewModel.userSignedIn?.name, "TEST")
+		}
+	}
+	
+	func testFailLogUser() async throws {
+		
+		let test = await BaseTest<SignUpViewModel, SignUpPresenter, SignUpInteractor>()
+		
+		await test.fire { interactor in
+			DispatchQueue.main.async {
+				interactor.userWorker = MockUserWorker()
+				interactor.signUp(mail: nil, name: nil, password: nil)
+			}
+			
+			DispatchQueue.main.async {
+				XCTAssertEqual(test.viewModel.userSignedIn, nil)
+			}
+		}
+	}
+}
