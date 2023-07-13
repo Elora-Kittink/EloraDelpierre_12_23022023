e29a8cccbc4039335c6cf6fb1603ba0fcdc3f645
Elora Delpierre <edelpierre@exomind.fr>
2023-07-13 17:28:07 +0200

    implementing BaseTableView

--- FosterApp/Modules/LitterHistory/LitterHistoryViewController.swift
+++ FosterApp/Modules/LitterHistory/LitterHistoryViewController.swift
@@ -12,16 +12,24 @@ class LitterHistoryViewController: BaseViewController<LitterHistoryViewModel,
 	
 	// MARK: - Outlets
     @IBOutlet private weak var litterHistoryTableView: UITableView!
-    @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet private weak var headerLabel: UILabel!
     
 	// MARK: - Variables
     var user: User!
-    
+//	TODO: comprendre tout ça
+	lazy var  littersTableView: BaseTableView<LitterCell, Litter> = {
+	let littersTableView =  BaseTableView <LitterCell, Litter> (didSelect: didSelect(item:at:))
+	return littersTableView
+	}() // homework, Why did we change it to lazy var and into a self executed closure?
+	
+	let litters = [Litter(isOngoing: true, rescueDate: "aujourd'hui"), Litter(isOngoing: true, rescueDate: "demain"), Litter(isOngoing: true, rescueDate: "hier")]
 	// MARK: - View life cycle
 	override func viewDidLoad() {
+		self.interactor.refresh(user: user)
 		super.viewDidLoad()
-        self.litterHistoryTableView.delegate = self
-        self.litterHistoryTableView.dataSource = self
+		self.setupUI()
+//		littersTableView.items = self.viewModel.litters ?? []
+		littersTableView.items = litters
 	}
 	
     override func viewWillAppear(_ animated: Bool) {
@@ -33,8 +41,21 @@ class LitterHistoryViewController: BaseViewController<LitterHistoryViewModel,
 	// MARK: - Refresh
 	override func refreshUI() {
 		super.refreshUI()
-        litterHistoryTableView.reloadData()
-        self.headerLabel.text = self.viewModel.headerText
+		littersTableView.reloadData()
+//        litterHistoryTableView.reloadData()
+//        self.headerLabel.text = self.viewModel.headerText
+	}
+	
+	func setupUI() {
+		self.view.addSubview(littersTableView)
+		littersTableView.translatesAutoresizingMaskIntoConstraints = false
+		littersTableView.backgroundColor = UIColor.purple
+		NSLayoutConstraint.activate([
+			littersTableView.topAnchor.constraint(equalTo: self.view.topAnchor),
+			littersTableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+			littersTableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+			littersTableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor )
+		])
 	}
 
 	// MARK: - Actions
@@ -47,44 +68,56 @@ class LitterHistoryViewController: BaseViewController<LitterHistoryViewModel,
 		}
         navigationController?.pushViewController(vc, animated: true)
     }
-}
-
-
-extension LitterHistoryViewController: UITableViewDataSource, UITableViewDelegate {
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        min(tableView.frame.height / 1.5, 50)
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        guard let litters = self.viewModel.litters else {
-            return 0
-        }
-       return litters.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "litterCell", for: indexPath)
-        self.interactor.refreshCell(litter: self.viewModel.litters?[indexPath.row])
-        cell.textLabel?.text = self.viewModel.kittenList
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        guard let litterId = self.viewModel.litters?[indexPath.row].id else {
-            return
-        }
+	
+	func didSelect(item: Litter, at indexPath: IndexPath) {
+		
 		let vc = LitterViewController.fromStoryboard { vc in
 			vc.user = self.user
-			vc.litterId = litterId
+			vc.litterId = item.id
 			vc.isDisplayMode = true
 			vc.isCreateMode = false
 			vc.isEditMode = false
 		}
-        navigationController?.pushViewController(vc, animated: true)
-    }
+		navigationController?.pushViewController(vc, animated: true)
+	}
 }
+
+
+//extension LitterHistoryViewController: UITableViewDataSource, UITableViewDelegate {
+//
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        min(tableView.frame.height / 1.5, 50)
+//    }
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        guard let litters = self.viewModel.litters else {
+//            return 0
+//        }
+//       return litters.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//
+//        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "litterCell", for: indexPath)
+//        self.interactor.refreshCell(litter: self.viewModel.litters?[indexPath.row])
+//        cell.textLabel?.text = self.viewModel.kittenList
+//        return cell
+//    }
+//
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        guard let litterId = self.viewModel.litters?[indexPath.row].id else {
+//            return
+//        }
+//		let vc = LitterViewController.fromStoryboard { vc in
+//			vc.user = self.user
+//			vc.litterId = litterId
+//			vc.isDisplayMode = true
+//			vc.isCreateMode = false
+//			vc.isEditMode = false
+//		}
+//        navigationController?.pushViewController(vc, animated: true)
+//    }
+//}
 
 extension LitterHistoryViewController: StoryboardProtocol {
     static var storyboardName: String {
