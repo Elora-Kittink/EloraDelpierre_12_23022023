639bccf72f699ca75850913b294b215751b4c8e1
Elora Delpierre <edelpierre@exomind.fr>
2023-09-28 17:29:43 +0200

    [Home] pimp UI

--- FosterApp/Modules/Home/HomeViewController.swift
+++ FosterApp/Modules/Home/HomeViewController.swift
@@ -8,23 +8,29 @@ import UIKit
 import FirebaseCore
 import FirebaseAuth
 
-class HomeViewController: BaseViewController
-<
-	HomeViewModel,
-	HomePresenter,
-	HomeInteractor
-> {
+class HomeViewController: BaseViewController< HomeViewModel, HomePresenter, HomeInteractor> {
     
 	// MARK: - Outlets
-    @IBOutlet private weak var button: UIButton!
+
     @IBOutlet private weak var logOutbutton: UIButton!
-    @IBOutlet private weak var advicesButton: UIButton!
+
     @IBOutlet private weak var welcomeLabel: UILabel!
 	
+	@IBOutlet weak var adminTile: UIView!
+	@IBOutlet weak var littersTiles: UIView!
+	@IBOutlet weak var advicesTiles: UIView!
+	@IBOutlet weak var galerieTile: UIView!
+	@IBOutlet weak var calendarTile: UIView!
 	// MARK: - View life cycle
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		self.adminTile.layer.cornerRadius = 12
+		self.advicesTiles.layer.cornerRadius = 12
+		self.littersTiles.layer.cornerRadius = 12
+		self.galerieTile.layer.cornerRadius = 12
+		self.calendarTile.layer.cornerRadius = 12
+		
 		self.interactor.userIsConnected()
 		NotificationCenter.default.addObserver(forName: NSNotification.Name("userLogged"),
 											   object: nil,
@@ -37,23 +43,23 @@ class HomeViewController: BaseViewController
 	// MARK: - Refresh
 	override func refreshUI() {
 		super.refreshUI()
-        self.welcomeLabel.text = "Welcome \(self.viewModel.user?.name ?? "")"
+        self.welcomeLabel.text = "Bienvenu \(self.viewModel.user?.name ?? "")"
 	}
 
 	// MARK: - Actions
-    @IBAction private func litterButtonAction() {
-
+    
+	@IBAction func didTapLittersTile(_ sender: UITapGestureRecognizer) {
 		let vc = LitterHistoryViewController.fromStoryboard { vc in
 			vc.user = self.viewModel.user
 		}
-        navigationController?.pushViewController(vc, animated: true)
-    }
-    
-    @IBAction private func advicesButtonAction() {
-        let vc = AdvicesViewController.fromStoryboard()
-        navigationController?.pushViewController(vc, animated: true)
-    }
-    
+		navigationController?.pushViewController(vc, animated: true)
+	}
+	
+	@IBAction func didTapAdvicesTile(_ sender: Any) {
+		let vc = AdvicesViewController.fromStoryboard()
+		navigationController?.pushViewController(vc, animated: true)
+	}
+
     @IBAction private func logOut() {
         self.interactor.logOut()
     }
